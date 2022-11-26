46b556e9b4bac761cd645af5c33e2122a5ba0015
emiliano <87916296+fishstraws@users.noreply.github.com>
2022-11-25 22:13:20 -0600

    add games data project

---
+++ games-data/sandbox-crud-operations-query.sql
@@ -0,0 +1,7 @@
+--INSERT INTO sandbox(int64_value, bool_value, string_value) VALUES (45433456, 1, 'hi sarah');
+
+--UPDATE sandbox SET double_value = -2.455442 WHERE id=181;
+
+--DELETE FROM sandbox WHERE id = 90;
+
+SELECT * FROM sandbox;
